bb898166d5483725baeced8f2a85d940f91640ce
SireeshaK19 <17691a05d3@mits.ac.in>
2020-02-26 20:26:40 +0530

    done

---
+++ com.task5/src/main/java/P_layer/Presented.java
@@ -0,0 +1,7 @@
+package P_layer;
+
+public interface Presented {
+	int Compound_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+	int Simple_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+
+}
---
+++ com.task5/src/main/java/client/Constuction.java
@@ -0,0 +1,5 @@
+package client;
+
+public interface Constuction {
+	String Constructions_Materials(String Material_Standard,int Area );
+}
